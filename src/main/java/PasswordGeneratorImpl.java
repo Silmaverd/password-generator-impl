f20bfbdc44a89b767b060e7f250bf69a053b0478
prsen <sen.przemyslaw@gmail.com>
2019-03-18 20:08:41 +0100

    v1

---
+++ src/main/java/PasswordGeneratorImpl.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class PasswordGeneratorImpl implements PasswordGeneratorService {
+
+    public String generate(PasswordGeneratorParameters passwordGeneratorParameters) {
+        return null;
+    }
+
+    public List<String> generate(PasswordGeneratorParameters passwordGeneratorParameters, int i) {
+        return null;
+    }
+}
